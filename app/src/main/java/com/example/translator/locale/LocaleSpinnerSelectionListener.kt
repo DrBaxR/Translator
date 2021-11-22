a1dde2e4d7e893f14c4065afbd933e62f72251cd
Alexandru Florea <alexflorea16000@gmail.com>
2021-11-22 16:56:19 +0200

    Voice page layout

--- app/src/main/java/com/example/translator/locale/LocaleSpinnerSelectionListener.kt
+++ app/src/main/java/com/example/translator/locale/LocaleSpinnerSelectionListener.kt
@@ -5,13 +5,37 @@ import android.widget.AdapterView
 import com.example.translator.state.LocaleState
 import java.util.*
 
-class LocaleSpinnerSelectionListener(
-    private val locales: List<Locale>
-) : AdapterView.OnItemSelectedListener {
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        LocaleState.selectedLocale = locales[position]
+open class LocaleSpinnerSelectionListener(private val localeToSet: String) :
+    AdapterView.OnItemSelectedListener {
+    companion object {
+        var locales: List<Locale> = getUniqueLocales()
+
+        private fun getUniqueLocales(): List<Locale> {
+            val uniqueLanguageLocales = mutableListOf<Locale>()
+            Locale.getAvailableLocales().forEach { locale ->
+                if (!uniqueLanguageLocales.any { it.displayLanguage == locale.displayLanguage }) {
+                    uniqueLanguageLocales.add(locale)
+                }
+            }
+
+            uniqueLanguageLocales.sortWith(
+                compareBy(
+                    { it.displayLanguage },
+                    { it.displayLanguage })
+            )
+
+            return uniqueLanguageLocales
+        }
     }
 
-    override fun onNothingSelected(parent: AdapterView<*>?) {  }
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (localeToSet) {
+            "s1" -> LocaleState.selectedSpeechLocale1 = locales[position]
+            "s2" -> LocaleState.selectedSpeechLocale2 = locales[position]
+        }
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
 
 }
--- app/src/main/java/com/example/translator/state/LocaleState.kt
+++ app/src/main/java/com/example/translator/state/LocaleState.kt
@@ -1,9 +1,14 @@
 package com.example.translator.state
 
+import com.example.translator.services.GoogleApiTranslator
 import java.util.*
 
 class LocaleState {
     companion object {
-        var selectedLocale: Locale = Locale.getDefault()
+        val googleApi = GoogleApiTranslator()
+
+        var selectedSpeechLocale1: Locale = Locale.getDefault()
+        var selectedSpeechLocale2: Locale = Locale.getDefault()
+
     }
 }
