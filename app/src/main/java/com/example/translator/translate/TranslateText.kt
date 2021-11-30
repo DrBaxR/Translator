bb806ea1085654463fefd877dcced4e7a904cef7
Marian Francescu <marian.francescu98@gmail.com>
2021-11-30 13:14:31 +0200

    Modified the screen of image to text

--- app/src/main/java/com/example/translator/translate/TranslateText.kt
+++ app/src/main/java/com/example/translator/translate/TranslateText.kt
@@ -2,16 +2,19 @@ package com.example.translator.translate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.core.view.get
 import com.example.translator.R
 import com.example.translator.services.GoogleApiTranslator
+import com.google.android.material.textfield.TextInputLayout
 
 class TranslateText : AppCompatActivity() {
 
-    private lateinit var textView: TextView
+    private lateinit var textView: TextInputLayout
+    private lateinit var textViewTranslated: TextInputLayout
     private var googleApi = GoogleApiTranslator()
     private lateinit var spinner: Spinner
     private lateinit var button: Button
@@ -21,15 +24,16 @@ class TranslateText : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_translate_text)
+        setContentView(R.layout.fragment_text)
 
-        textView = findViewById(R.id.textView1)
-        spinner = findViewById(R.id.spinner)
-        button = findViewById(R.id.button)
+        textView = findViewById(R.id.tTextField1)
+        textViewTranslated = findViewById(R.id.tTextField2)
+        spinner = findViewById(R.id.tSpinner)
+        button = findViewById(R.id.tButton)
 
         val bundle = intent.extras
         var message = bundle!!.getString("message")
-        textView.text = message
+        textView.editText?.text = SpannableStringBuilder(message)
 
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayOptions)
 
@@ -37,8 +41,8 @@ class TranslateText : AppCompatActivity() {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 button.setOnClickListener {
                     googleApi.getTranslateService(context = baseContext)
-                    message = googleApi.translate(message!!, arraySelected[p2])
-                    textView.text = message
+                    var translatedMessage = googleApi.translate(message!!, arraySelected[p2])
+                    textViewTranslated.editText?.text = SpannableStringBuilder(translatedMessage)
                 }
             }
             override fun onNothingSelected(p0: AdapterView<*>?) {
