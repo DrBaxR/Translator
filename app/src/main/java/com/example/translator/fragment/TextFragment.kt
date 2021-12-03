9286221abd3ee99fa52c337e0a892036bc001828
Marian Francescu <marian.francescu98@gmail.com>
2021-12-03 14:03:07 +0200

    Implemented functionality to limit the number of translate attempt for a non premium user

--- app/src/main/java/com/example/translator/fragment/TextFragment.kt
+++ app/src/main/java/com/example/translator/fragment/TextFragment.kt
@@ -1,7 +1,9 @@
 package com.example.translator.fragment
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.text.SpannableStringBuilder
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,12 +16,32 @@ import com.example.translator.locale.LocaleSpinnerSelectionListener
 import com.example.translator.locale.LocaleSpinnerSelectionListenerWithExtra
 import com.example.translator.state.LocaleState
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.properties.Delegates
+
 
 class TextFragment : Fragment() {
+    private lateinit var databaseReference: DatabaseReference
+    private var counter by Delegates.notNull<Int>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        databaseReference = FirebaseDatabase.getInstance().reference
+        val uid = FirebaseAuth.getInstance().uid
+        val current = LocalDate.now()
+
+        initializeUserData(uid, current);
+        readCounterFromDatabase(uid)
+
         val view = inflater.inflate(R.layout.fragment_text, container, false)
 
         if (view != null) {
@@ -32,15 +54,68 @@ class TextFragment : Fragment() {
             spinner.adapter = LocaleAdapter(view.context, LocaleSpinnerSelectionListener.locales)
 
             val button = view.findViewById<Button>(R.id.tButton)
+
             button.setOnClickListener {
-                translateText(
-                    tTextField1.editText?.text?.toString(),
-                    tTextField2
-                )
+                if(counter < 3) {
+                    counter++;
+                    databaseReference.child("Users").child("$uid").child("Count").setValue(counter)
+
+                    translateText(
+                        tTextField1.editText?.text?.toString(),
+                        tTextField2
+                    )
+                }
             }
         }
 
         return view
+    }
+
+    private fun initializeUserData(uid: String?, current: LocalDate){
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val dateValue = snapshot.child("Users").child("$uid").child("Day").getValue()
+                lateinit var date: LocalDate
+
+                if(dateValue == null){
+                    getInitializationData(databaseReference, uid, current)
+                }
+                else
+                {
+                    date = LocalDate.parse(dateValue.toString(), DateTimeFormatter.ISO_LOCAL_DATE)
+
+                    if(current.isAfter(date))
+                    {
+                        getInitializationData(databaseReference, uid, current)
+                    }
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "onCancelled", error.toException());
+            }
+        })
+    }
+
+    private fun getInitializationData(databaseReference: DatabaseReference, uid: String?, current: LocalDate){
+        counter = 0;
+        databaseReference.child("Users").child("$uid").child("Day").setValue("$current")
+        databaseReference.child("Users").child("$uid").child("Count").setValue(counter)
+        databaseReference.child("Users").child("$uid").child("isPremium").setValue(false)
+    }
+
+    private fun readCounterFromDatabase(uid: String?) {
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val counterValue = snapshot.child("Users").child("$uid").child("Count").getValue()
+                counter = counterValue.toString().toInt()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "onCancelled", error.toException());
+            }
+        })
     }
 
     private fun translateText(text: String?, textField: TextInputLayout) {
