eb72595ac05c9d7c6f3724972885c307ef1d2fad
Alexandru Florea <alexflorea16000@gmail.com>
2021-12-14 15:36:43 +0200

    Swapped speech spinners to autocomplete text views

--- app/src/main/java/com/example/translator/fragment/SpeechFragment.kt
+++ app/src/main/java/com/example/translator/fragment/SpeechFragment.kt
@@ -9,14 +9,13 @@ import android.view.ViewGroup
 import android.widget.*
 import com.example.translator.R
 import com.example.translator.contracts.RecognizeSpeech
-import com.example.translator.locale.LocaleAdapter
-import com.example.translator.locale.LocaleSpinnerSelectionListener
-import com.example.translator.locale.LocaleSpinnerSelectionListenerWithExtra
+import com.example.translator.locale.*
 import com.example.translator.services.PremiumService
 import com.example.translator.state.AppState
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class SpeechFragment : Fragment() {
     private var sTextField1: TextInputLayout? = null
@@ -68,18 +67,32 @@ class SpeechFragment : Fragment() {
             }
         }
 
-        val spinner1 = view?.findViewById<Spinner>(R.id.slSpinner1)
-        spinner1?.onItemSelectedListener = LocaleSpinnerSelectionListenerWithExtra("s1") {
-            button1?.text = it.displayLanguage
-            resetTextViews(sTextField1, sTextField2)
+        val autocomplete1 = view.findViewById<AutoCompleteTextView>(R.id.slSpinner1)
+        val adapter1 = AutocompleteLocaleAdapter(view.context, AutocompleteLocale.locales)
+        autocomplete1.setAdapter(adapter1)
+        autocomplete1.setOnItemClickListener { _, _, pos, _ ->
+            val selectedItem = adapter1.getItem(pos)
+            if (selectedItem != null) {
+                AppState.selectedSpeechLocale1 = Locale(selectedItem.locale)
+                button1?.text = selectedItem.language
+                resetTextViews(sTextField1, sTextField2)
+            }
         }
-        spinner1?.adapter = LocaleAdapter(view.context, LocaleSpinnerSelectionListener.locales)
-        val spinner2 = view?.findViewById<Spinner>(R.id.slSpinner2)
-        spinner2?.onItemSelectedListener = LocaleSpinnerSelectionListenerWithExtra("s2") {
-            button2?.text = it.displayLanguage
-            resetTextViews(sTextField1, sTextField2)
+
+        val autocomplete2 = view.findViewById<AutoCompleteTextView>(R.id.slSpinner2)
+        val adapter2 = AutocompleteLocaleAdapter(view.context, AutocompleteLocale.locales)
+        autocomplete2.setAdapter(adapter2)
+        autocomplete2.setOnItemClickListener { _, _, pos, _ ->
+            val selectedItem = adapter2.getItem(pos)
+            if (selectedItem != null) {
+                AppState.selectedSpeechLocale2 = Locale(selectedItem.locale)
+                button2?.text = selectedItem.language
+                resetTextViews(sTextField1, sTextField2)
+            }
         }
-        spinner2?.adapter = LocaleAdapter(view.context, LocaleSpinnerSelectionListener.locales)
+
+        autocomplete1.text = SpannableStringBuilder(AppState.selectedSpeechLocale1.displayLanguage)
+        autocomplete2.text = SpannableStringBuilder(AppState.selectedSpeechLocale2.displayLanguage)
 
         return view
     }
